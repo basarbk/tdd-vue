afeacb8b63df5da9c7ba4b7d1a7e5b8b64e1b20a
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-27 01:20:44 +0300

    0201-first-component

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,8 +2,52 @@ import SignUpPage from "./SignUpPage.vue";
 import { render, screen } from "@testing-library/vue";
 import "@testing-library/jest-dom";
 
-it("has Sign Up Header", () => {
-  render(SignUpPage);
-  const header = screen.queryByRole("heading", { name: "Sign Up" });
-  expect(header).toBeInTheDocument();
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has Sign Up Header", () => {
+      render(SignUpPage);
+      const header = screen.queryByRole("heading", { name: "Sign Up" });
+      expect(header).toBeInTheDocument();
+    });
+    it("has username input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Username");
+      expect(input).toBeInTheDocument();
+    });
+    it("has email input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("E-mail");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Password");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Password Repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has password type for password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.queryByLabelText("Password Repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has Sign Up button", () => {
+      render(SignUpPage);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
+    });
+    it("disables the button initially", () => {
+      render(SignUpPage);
+      const button = screen.queryByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
+    });
+  });
 });
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,3 +1,16 @@
 <template>
   <h1>Sign Up</h1>
+  <label for="username">Username</label>
+  <input id="username" />
+
+  <label for="e-mail">E-mail</label>
+  <input id="e-mail" />
+
+  <label for="password">Password</label>
+  <input id="password" type="password" />
+
+  <label for="password-repeat">Password Repeat</label>
+  <input id="password-repeat" type="password" />
+
+  <button disabled>Sign Up</button>
 </template>
