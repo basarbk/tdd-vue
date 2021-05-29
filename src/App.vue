2c7d501b16fce68a9c4b7e3190abf2ca09ac2320
Basar Buyukkahraman <basarbk@yahoo.com>
2021-05-29 16:09:56 +0300

    0206-style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <SignUpPage />
+  <div class="container">
+    <SignUpPage />
+  </div>
 </template>
 
 <script>
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,20 +1,50 @@
 <template>
-  <form>
-    <h1>Sign Up</h1>
-    <label for="username">Username</label>
-    <input id="username" v-model="username" />
-
-    <label for="e-mail">E-mail</label>
-    <input id="e-mail" v-model="email" />
-
-    <label for="password">Password</label>
-    <input id="password" type="password" v-model="password" />
-
-    <label for="password-repeat">Password Repeat</label>
-    <input id="password-repeat" type="password" v-model="passwordRepeat" />
-
-    <button :disabled="isDisabled" @click.prevent="submit">Sign Up</button>
-  </form>
+  <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+    <form class="card mt-5">
+      <div class="card-header">
+        <h1 class="text-center">Sign Up</h1>
+      </div>
+      <div class="card-body">
+        <div class="mb-3">
+          <label for="username" class="form-label">Username</label>
+          <input id="username" v-model="username" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label for="e-mail" class="form-label">E-mail</label>
+          <input id="e-mail" v-model="email" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input
+            id="password"
+            type="password"
+            v-model="password"
+            class="form-control"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password-repeat" class="form-label"
+            >Password Repeat</label
+          >
+          <input
+            id="password-repeat"
+            type="password"
+            v-model="passwordRepeat"
+            class="form-control"
+          />
+        </div>
+        <div class="text-center">
+          <button
+            class="btn btn-primary"
+            :disabled="isDisabled"
+            @click.prevent="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 import axios from "axios";
