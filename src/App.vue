439c64c995ce3b04211e506e3c6404de3d15b743
Basar Buyukkahraman <basarbk@yahoo.com>
2021-06-19 00:18:50 +0300

    0802-navbar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,5 @@
 <template>
-  <div class="shadow-sm bg-light mb-3">
-    <nav class="navbar navbar-expand navbar-light container">
-      <div class="container-fluid p-0">
-        <router-link class="navbar-brand" to="/" title="Home">
-          <img src="./assets/hoaxify.png" width="60" alt="Hoaxify Logo" />
-
-          Hoaxify</router-link
-        >
-        <ul class="navbar-nav ml-auto">
-          <router-link
-            class="nav-link"
-            to="/signup"
-            v-if="!$store.state.isLoggedIn"
-            >{{ $t("signUp") }}</router-link
-          >
-          <router-link
-            class="nav-link"
-            to="/login"
-            v-if="!$store.state.isLoggedIn"
-            >Login</router-link
-          >
-          <router-link
-            class="nav-link"
-            :to="'/user/' + $store.state.id"
-            v-if="$store.state.isLoggedIn"
-            >My Profile</router-link
-          >
-        </ul>
-      </div>
-    </nav>
-  </div>
+  <NavBar />
   <div class="container">
     <router-view />
     <LanguageSelector />
@@ -38,10 +8,12 @@
 
 <script>
 import LanguageSelector from "./components/LanguageSelector";
+import NavBar from "./components/NavBar";
 export default {
   name: "App",
   components: {
     LanguageSelector,
+    NavBar,
   },
   data() {
     return {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="shadow-sm bg-light mb-3">
+    <nav class="navbar navbar-expand navbar-light container">
+      <div class="container-fluid p-0">
+        <router-link class="navbar-brand" to="/" title="Home">
+          <img src="../assets/hoaxify.png" width="60" alt="Hoaxify Logo" />
+
+          Hoaxify</router-link
+        >
+        <ul class="navbar-nav ml-auto">
+          <router-link
+            class="nav-link"
+            to="/signup"
+            v-if="!$store.state.isLoggedIn"
+            >{{ $t("signUp") }}</router-link
+          >
+          <router-link
+            class="nav-link"
+            to="/login"
+            v-if="!$store.state.isLoggedIn"
+            >Login</router-link
+          >
+          <router-link
+            class="nav-link"
+            :to="'/user/' + $store.state.id"
+            v-if="$store.state.isLoggedIn"
+            >My Profile</router-link
+          >
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
